e8552a1c8406a26916bde80c38e9f5a78cb74e90
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-03-14 20:27:07 +0500

    [FIX] Change the structure of the repository

--- wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import contract_content_wizard
-from . import contract_publish_wizard
-from . import contract_version_sign_wizard
-from . import contract_line_wizard
-from . import confirm_delete_wizard
-from . import contract_section_wizard
-from . import contract_version_creation_wizard
